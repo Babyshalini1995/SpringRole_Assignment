c2b417c18e0b92565184507ec8233095127c4a5f
Shalini <baby.shalini@tesco.com>
2019-07-31 23:17:46 +0530

    Setting up the folder structure for different views

---
+++ assignment-app/src/containers/DetailView/DetailPage.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Detail Page
+ *
+ */
+
+import React from 'react';
+
+export default class DetailPage extends React.Component{
+    render() {
+        return (
+            <div>
+                Detail Page
+            </div>
+        );
+    }
+}
---
+++ assignment-app/src/containers/TableView/TablePage.js
@@ -0,0 +1,17 @@
+/*
+ *
+ * Table Page
+ *
+ */
+
+import React from 'react';
+
+export default class TablePage extends React.Component{
+    render() {
+        return (
+            <div>
+                Table Page
+            </div>
+        );
+    }
+}

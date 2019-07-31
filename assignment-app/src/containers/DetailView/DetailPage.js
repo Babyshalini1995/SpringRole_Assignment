638acfa03088322d6a2c6ad6828b38dbc10fb494
Shalini <baby.shalini@tesco.com>
2019-08-01 01:35:16 +0530

    Added API call to get the user data in detail page

--- assignment-app/src/containers/DetailView/DetailPage.js
+++ assignment-app/src/containers/DetailView/DetailPage.js
@@ -7,10 +7,35 @@
 import React from 'react';
 
 export default class DetailPage extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state= {
+            userData: []
+        }
+
+    }
+
+    componentDidMount() {
+        const { match: { params } } = this.props;
+
+        fetch(`http://demo9197058.mockable.io/users`, {
+            mode: "cors",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then( response => response.json() )
+            .then( data => {
+                console.log(data.filter(user => {return params.userId.includes(user.id)}))
+            }
+            );
+
+    }
     render() {
         return (
             <div>
-                Detail Page
+                DetailPage
             </div>
         );
     }
